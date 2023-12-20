9bb98c39af9051838a212324cab88a325b77985a
丁保磊 <1348909544@qq.com>
2023-12-20 19:29:53 +0800

    axios first

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    cacheRequestConfig: [],
   },
   getters: {
+    cacheRequestConfig (state) {
+      return state.cacheRequestConfig
+    },
   },
   mutations: {
+    SET_CACHE_REQUEST_CONFIG (state, payload) {
+      state.cacheRequestConfig = payload
+    },
   },
-  actions: {
-  },
-  modules: {
-  },
+  actions: {},
+  modules: {},
 })
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button @click="testAxios">testAxios</el-button>
+    <el-button @click="testAxiosToo">testAxiosToo</el-button>
   </div>
 </template>
+
+<script>
+import _axios from '@/common/utils/_axios'
+import { mapGetters, mapMutations } from 'vuex'
+
+export default {
+  name: 'about-view',
+  computed: {
+    ...mapGetters([ 'cacheRequestConfig' ]),
+  },
+  methods: {
+    testAxios () {
+      _axios.get('http://localhost:7001').then(res => {
+        console.log('testAxios: ', res)
+      }).catch(e => {
+        console.error(e + 'testAxios')
+      })
+    },
+    testAxiosToo () {
+      this.cacheRequestConfig.forEach(config => {
+        _axios(config)
+      })
+    },
+    ...mapMutations({
+      setCacheRequestConfig: 'SET_CACHE_REQUEST_CONFIG',
+    }),
+  },
+}
+</script>
